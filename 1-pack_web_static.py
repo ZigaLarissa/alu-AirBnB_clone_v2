951ab3fe6795d761c34702367332fb0fba81366e
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-06-23 19:40:46 +0200

    woo

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Fabric script that creates and distributes an archive to your web servers"""
-from invoke import run
+import os
 from datetime import datetime
+from fabric import local
 
 
 def do_pack():
@@ -11,7 +12,7 @@ def do_pack():
     archive_name = "web_static_{}.tgz".format(timestamp)
     archive_path = "versions/{}".format(archive_name)
 
-    run("mkdir -p versions")
-    run("tar -czvf {} web_static".format(archive_path))
+    os.makedirs("versions", exist_ok=True)
+    local("tar -czvf {} web_static".format(archive_path))
 
     return archive_path
