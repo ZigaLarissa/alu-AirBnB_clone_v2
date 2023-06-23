3a050d707d7519420723896a227c12d68abc1f9d
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-06-23 19:37:00 +0200

    using invoke

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """Fabric script that creates and distributes an archive to your web servers"""
-
-from fabric import Connection
+from invoke import run
 from datetime import datetime
-import os
 
 
 def do_pack():
-    """Generate a .tgz archive from the contents of the web_static folder."""
+    """Generates a .tgz archive from the contents of the web_static folder."""
     now = datetime.now()
     timestamp = now.strftime("%Y%m%d%H%M%S")
     archive_name = "web_static_{}.tgz".format(timestamp)
     archive_path = "versions/{}".format(archive_name)
 
-    # Create the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+    run("mkdir -p versions")
+    run("tar -czvf {} web_static".format(archive_path))
 
-    try:
-        # Create the .tgz archive
-        with Connection('localhost') as conn:
-            conn.local("tar -czvf {} web_static".format(archive_path))
-        return archive_path
-    except Exception as e:
-        print("Error: {}".format(e))
-        return None
+    return archive_path
