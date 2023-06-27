3fd91c9aa10949126588f4874174df8b0699621f
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-27 18:40:58 +0200

    documenting every method

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Route handle for root URL"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
+    """Route handle for /hbnb URL"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def hello(text):
+    """Route handle for /c/<text>URL"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
