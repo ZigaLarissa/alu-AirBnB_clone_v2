50489a58677a61c7d776fc0540d6b145753eb49b
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-27 18:35:16 +0200

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello(text):
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0000000', port=5000)
