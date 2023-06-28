9530eb8518d359dd97b72482209d3c32f98be363
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-28 18:55:10 +0200

    task 9 trial 1

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,10 +8,10 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("cities_by_states", strict_slashes=False)
+@app.route('cities_by_states', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
