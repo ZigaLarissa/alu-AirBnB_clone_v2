f30f8e2a3ada604ac12dea061c7f0a0ae3f72e08
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-28 18:36:32 +0200

    task 8 trial 1

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown(exceptions):
+    storage.close()   
+    
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000) 
