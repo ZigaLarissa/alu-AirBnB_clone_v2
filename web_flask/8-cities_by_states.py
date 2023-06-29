25c956f42d8c900169bc26ad3fdb9a6435334e4b
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-29 12:13:37 +0200

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,29 +10,11 @@ from flask import render_template
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-def state_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_route():
     """Comment"""
-    states = storage.all('State').values()
-    return render_template(
-        "9-states.html",
-        states=states,
-        condition="states_list")
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_by_id(id):
-    """Comment"""
-    all_states = storage.all('State')
-    key = "State.{}".format(id)
-    try:
-        state = all_states[key]
-        return render_template(
-            '9-states.html',
-            state=state,
-            condition="state_id")
-    except:
-        return render_template('9-states.html', condition="not_found")
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
