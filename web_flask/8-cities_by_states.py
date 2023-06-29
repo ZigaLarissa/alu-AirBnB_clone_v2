d60acce4162bfde1246ee3c0c5fdb9b88c7d3eb0
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-29 11:18:19 +0200

    task 8,9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web app"""
-from flask import Flask, render_template
+
+"""Starts a Flask web application"""
+
 from models import storage
 from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State)
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-states_list.html', states=sorted_states)
+def cities_route():
+    """Comment"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
 def teardown(self):
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
