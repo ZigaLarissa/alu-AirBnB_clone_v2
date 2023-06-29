ff1fd6027b64e10dee3fa93a9d47f002a826b00f
mutabazichristian <c.mutabazi@alustudent.com>
2023-06-29 11:30:26 +0200

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -26,3 +26,6 @@ def states_id(id):
     if state is None:
         return jsonify({"error": "Not found"}), 404
     return jsonify(state.to_dict())
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
